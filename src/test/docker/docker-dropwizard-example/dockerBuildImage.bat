9d61a93046f38efd03348fa328cc817b7ad2a9d5
Joe Pusateri <joseph.pusateri@netuitive.com>
2017-02-14 13:45:41 -0600

    Deleted unnecessary helper scripts

--- src/test/docker/docker-dropwizard-example/dockerBuildImage.bat
+++
@@ -1,1 +0,0 @@
-docker build -t example/dropwizard:latest .
--- src/test/docker/docker-dropwizard-example/go.bat
+++
@@ -1,2 +0,0 @@
-call gradle clean oneJar
-java -jar -Done-jar.silent=true build/libs/docker-dropwizard-example-standalone.jar server config.yml
